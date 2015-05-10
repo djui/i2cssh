cd4be9c1fd182fb643353c58750e7feae14146a7
Uwe Dauernheim <uwe@dauernheim.net>
2015-05-11 00:27:14 +0200

    Bump Homebrew formula version to 1.3
    Change-Id: I3b2acfed30a54d1c37fe3eec74a03bae426a7258

--- i2cssh.rb
+++ i2cssh.rb
@@ -1,8 +1,8 @@
 class I2cssh < Formula
   homepage "https://github.com/djui/i2cssh"
   head "https://github.com/djui/i2cssh.git"
-  url "https://github.com/djui/i2cssh/archive/1.2.tar.gz"
-  sha256 "8dd1e3f6c00eae7882876848e460ac290a0d787de3275787d8ee1b6edddf55f9"
+  url "https://github.com/djui/i2cssh/archive/1.3.tar.gz"
+  sha256 "4f7b654399e664fad59262ca2de30cee1cf42e5d37c901e02ce31a1cef01e723"
 
   def install
     bin.install "i2cssh"
